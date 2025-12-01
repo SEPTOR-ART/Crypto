fc3b817e9d44e241dd558567ace10a4b9cba7c97
SEPTOR-ART <sirprist1@gmail.com>
2025-12-01 19:15:08 +0100

    Dashboard responsive: wrap section headers, reduce paddings on small screens, prevent horizontal overflow; add viewport-fit=cover

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
         <meta charSet="utf-8" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
